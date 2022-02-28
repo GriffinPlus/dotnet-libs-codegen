8f06479a02081b059238d0d9b1db2ea35186a3e3
Sascha Falk <sascha@falk-online.eu>
2022-02-28 08:42:04 +0100

    Add build.vs compatible solution

---
+++ src/CommonAssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyCompany("Griffin+")]
+[assembly: AssemblyTitle("Griffin+ Code Generation")]
+[assembly: AssemblyProduct("Griffin+ Library Suite")]
+[assembly: AssemblyCopyright("Copyright (c) 2022 Sascha Falk and Contributors")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
+[assembly: AssemblyInformationalVersion("--generated--")]
+
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
