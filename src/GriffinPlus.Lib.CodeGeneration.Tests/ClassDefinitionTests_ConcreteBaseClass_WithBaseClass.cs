151212ede0bb59366ff6e96ede06fb15c05aa55f
Sascha Falk <sascha@falk-online.eu>
2024-09-18 11:46:33 +0200

    Add test for ClassDefinition.AddEventOverride() with implementation callbacks

--- src/GriffinPlus.Lib.CodeGeneration.Tests/ClassDefinitionTests_ConcreteBaseClass_WithBaseClass.cs
+++ src/GriffinPlus.Lib.CodeGeneration.Tests/ClassDefinitionTests_ConcreteBaseClass_WithBaseClass.cs
@@ -7,8 +7,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using System.Reflection.Emit;
 
 using Xunit;
+
+// ReSharper disable MoveLocalFunctionAfterJumpStatement
 
 namespace GriffinPlus.Lib.CodeGeneration.Tests;
 
@@ -22,7 +25,7 @@ public abstract class ClassDefinitionTests_ConcreteBaseClass_WithBaseClass<TInhe
 {
 	#region Adding Events
 
-	#region AddEventOverride<T>(IInheritedEvent<T> eventToOverride, IEventImplementation<T> implementation)
+	#region Test Data
 
 	/// <summary>
 	/// Test data for tests targeting <see cref="ClassDefinition.AddEventOverride{T}(IInheritedEvent{T},IEventImplementation)"/>
@@ -62,6 +65,10 @@ public abstract class ClassDefinitionTests_ConcreteBaseClass_WithBaseClass<TInhe
 			static bool IsOverrideable(MethodInfo method) => method != null && method.IsVirtual && !method.IsFinal && (method.IsPublic || method.IsFamily);
 		}
 	}
+
+	#endregion
+
+	#region AddEventOverride<T>(IInheritedEvent<T> eventToOverride, IEventImplementation<T> implementation)
 
 	/// <summary>
 	/// Tests the <see cref="ClassDefinition.AddEventOverride{T}(IInheritedEvent{T},IEventImplementation)"/> method
@@ -150,9 +157,105 @@ public abstract class ClassDefinitionTests_ConcreteBaseClass_WithBaseClass<TInhe
 	/// Tests the
 	/// <see cref="ClassDefinition.AddEventOverride{T}(IInheritedEvent{T},EventAccessorImplementationCallback,EventAccessorImplementationCallback)"/> method.
 	/// </summary>
-	public void AddEventOverride_WithImplementationCallbacks()
+	/// <param name="name">Name of the event to override.</param>
+	/// <param name="expectedVisibility">Expected visibility of the overridden event.</param>
+	/// <param name="expectedEventHandlerType">Expected type of the overridden event.</param>
+	/// <param name="addEventRaiserMethod">
+	/// <c>true</c> to add the event raiser method;<br/>
+	/// otherwise <c>false</c>.
+	/// </param>
+	/// <param name="expectedEventRaiserReturnType">Expected return type of the generated event raiser method.</param>
+	/// <param name="expectedEventRaiserParameterTypes">Expected parameter types of the generated event raiser method.</param>
+	[Theory]
+	[MemberData(nameof(AddEventOverrideTestData))]
+	public void AddEventOverride_WithImplementationCallbacks(
+		string     name,
+		Visibility expectedVisibility,
+		Type       expectedEventHandlerType,
+		bool       addEventRaiserMethod,
+		Type       expectedEventRaiserReturnType,
+		Type[]     expectedEventRaiserParameterTypes)
 	{
-		// TODO: Implement...
+		// create a new type definition
+		ClassDefinition definition = CreateTypeDefinition();
+
+		// add a backing field for the event's multicast delegate
+		// (always of type EventHandler<EventArgs>, no need to test other types as this affects raising the event only)
+		IGeneratedField backingField = definition.AddField(expectedEventHandlerType, name: null, Visibility.Public);
+
+		// get the inherited event to override by its name
+		IInheritedEvent eventToOverride = definition.InheritedEvents.SingleOrDefault(x => x.Name == name);
+		Assert.NotNull(eventToOverride);
+
+		// get the AddEventOverride(...) method to test
+		MethodInfo addEventMethod = typeof(ClassDefinition)
+			.GetMethods(BindingFlags.Public | BindingFlags.Instance)
+			.Where(method => method.Name == nameof(ClassDefinition.AddEventOverride))
+			.Where(method => method.GetGenericArguments().Length == 1)
+			.Select(method => method.MakeGenericMethod(eventToOverride.EventHandlerType))
+			.Single(
+				method => method
+					.GetParameters()
+					.Select(parameter => parameter.ParameterType)
+					.SequenceEqual(
+					[
+						typeof(IInheritedEvent<>).MakeGenericType(eventToOverride.EventHandlerType),
+						typeof(EventAccessorImplementationCallback),
+						typeof(EventAccessorImplementationCallback)
+					]));
+
+		// invoke the method to add the event to the type definition
+		void ImplementGetAccessor(IGeneratedEvent @event, ILGenerator msilGenerator) => ImplementEventAccessor(@event, true, backingField.FieldBuilder, msilGenerator);
+		void ImplementSetAccessor(IGeneratedEvent @event, ILGenerator msilGenerator) => ImplementEventAccessor(@event, false, backingField.FieldBuilder, msilGenerator);
+		var addedEvent = (IGeneratedEvent)addEventMethod.Invoke(
+			definition,
+			[
+				eventToOverride,
+				(EventAccessorImplementationCallback)ImplementGetAccessor,
+				(EventAccessorImplementationCallback)ImplementSetAccessor
+			]);
+		Assert.NotNull(addedEvent);
+		Assert.Equal(EventKind.Override, addedEvent.Kind);
+		Assert.Equal(expectedVisibility, addedEvent.Visibility);
+		Assert.Equal(expectedEventHandlerType, addedEvent.EventHandlerType);
+		Assert.Null(addedEvent.Implementation);
+
+		// add an event raiser method to the type definition
+		// (should always just be: public void FireMyEvent();
+		const MethodKind kind = MethodKind.Normal;
+		const string eventRaiserName = "FireMyEvent";
+		Type eventRaiserReturnType = typeof(void);
+		Type[] eventRaiserParameterTypes = [];
+		const Visibility eventRaiserVisibility = Visibility.Public;
+		definition.AddMethod(
+			kind,
+			eventRaiserName,
+			eventRaiserReturnType,
+			eventRaiserParameterTypes,
+			eventRaiserVisibility,
+			(method, msilGenerator) => ImplementEventRaiserMethod(
+				method,
+				backingField.FieldBuilder,
+				addedEvent,
+				msilGenerator));
+
+		// create the defined type, check the result against the definition and create an instance of that type
+		Type type = definition.CreateType();
+		CheckTypeAgainstDefinition(type, definition);
+		object instance = Activator.CreateInstance(type);
+
+		// test the implementation of the event
+		// (the implemented event should behave the same as a standard event implementation, use the same test code...)
+		TestEventImplementation_Standard(
+			definition,
+			instance,
+			EventKind.Override,
+			eventToOverride.Name,
+			eventToOverride.EventHandlerType,
+			addEventRaiserMethod,
+			eventRaiserName,
+			expectedEventRaiserReturnType,
+			expectedEventRaiserParameterTypes);
 	}
 
 	#endregion
